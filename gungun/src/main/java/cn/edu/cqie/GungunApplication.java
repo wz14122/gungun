ea87ce40e01bd857006a2736e8a9479c1981992c
potato <potato@DESKTOP-2DB261L>
2020-12-28 13:53:38 +0800

    初始化项目

---
+++ gungun/src/main/java/cn/edu/cqie/GungunApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cqie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GungunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GungunApplication.class, args);
+	}
+
+}
---
+++ gungun/src/test/java/cn/edu/cqie/GungunApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cqie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GungunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
